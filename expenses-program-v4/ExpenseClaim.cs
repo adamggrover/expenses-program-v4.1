39a15fb50bc1769a34a3ee558dc65333945fdc4c
adamggrover <93373074+adamggrover@users.noreply.github.com>
2023-05-08 18:07:38 +0100

    Add project files.

---
+++ expenses-program-v4/ExpenseClaim.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+internal class ExpenseClaim
+{
+
+    // Fields
+    static int claimCount = 0;
+
+    private int claimId;
+    private int numberOfJourneys;
+    private double totalExpenses = 0;
+    private double totalTravelCost = 0;
+    // Create a list of the Journey objects
+    private List<Journey> Journeys = new List<Journey>();
+    private double totalExpenseClaim = 0;
+    private double totalNonRefundable = 0;
+    private double totalEmployeePayment = 0;
+
+
+    // Constructor
+    public ExpenseClaim()
+    {
+        ++claimCount;
+        this.claimId = claimCount;
+        
+
+    }
+
+    //--------Claim Id-----------------------------------//
+
+    // Getter method for Claim Id
+
+    public int GetClaimId()
+    {
+        return claimId;
+    }
+
+
+    //---------Journeys-----------------------------//
+
+    // Setter method for the number of journeys field    
+    public void SetNumberOfJourneys()
+    {
+        // Ask the user for the number of journeys completed
+        Console.Write("Please enter the number of journeys completed: ");
+
+        // Assign number of journeys user selected to local variable
+        this.numberOfJourneys = int.Parse(Console.ReadLine());
+
+    }
+
+    // Getter method for the number of journeys field
+    public int GetNumberOfJourneys()
+    {
+        return numberOfJourneys;
+    }
+
+
+    // Getter method for Journeys list
+
+    public List<Journey> GetJourneysList()
+    {
+        return Journeys;
+    }
+
+    // Setter method for adding Journey to Journeys list
+
+    public void AddJourneyToList(Journey journey)
+    {
+        Journeys.Add(journey);
+    }
+
+
+
+
+
+
+
+    //---------Total Expenses-----------------------------//
+
+
+    // Setter method for the total expenses field
+
+    public void AddToTotalExpenses(double expenseCost)
+    {
+        this.totalExpenses += expenseCost;
+    }
+
+
+    // Getter method for the total expenses field
+    public double GetTotalExpenses()
+    {
+        return totalExpenses;
+    }
+
+    //---------Total Travel Cost-----------------------------//
+
+    // Setter method for the total travel cost field
+
+    public void AddToTotalTravelCost(double travelCost)
+    {
+        this.totalTravelCost += travelCost;
+    }
+
+
+    // Getter method for the total expenses field
+    public double GetTotalTravelCost()
+    {
+        return totalTravelCost;
+    }
+
+    //---------Total Expense Claim-----------------------------//
+
+    // Setter method for the total travel cost field
+
+    public void AddToTotalExpenseClaim()
+    {
+        this.totalExpenseClaim = totalTravelCost + totalExpenses;
+    }
+
+
+    // Getter method for the total expenses field
+    public double GetTotalExpenseClaim()
+    {
+        return totalExpenseClaim;
+    }
+
+    //---------Total Non Refundable-----------------------------//
+
+    // Setter method for the total travel cost field
+
+    public void AddToTotalNonRefundable(double nonRefundable)
+    {
+        this.totalNonRefundable += nonRefundable;
+    }
+
+
+    // Getter method for the total expenses field
+    public double GetTotalNonRefundable()
+    {
+        return totalNonRefundable;
+    }
+
+
+    //---------Total Non Refundable-----------------------------//
+
+    // Setter method for the total travel cost field
+
+    public void SetTotalEmployeePayment()
+    {
+        this.totalEmployeePayment = totalExpenseClaim - totalNonRefundable;
+    }
+
+
+    // Getter method for the total expenses field
+    public double GetTotalEmployeePayment()
+    {
+        return totalEmployeePayment;
+    }
+
+
+    //---------Receipt Details--------------------------------------------------------------------------------------------------------//
+
+    // Method to generate the receipt details as a list of strings
+    public List<string> GetReceiptDetails()
+    {
+        // Create a new list to store the receipt details
+        List<string> details = new List<string>();
+
+        // Add the claim type, expense cost, travel cost, non-refundable amount, and total cost to the list of receipt details
+        
+
+        details.Add("\nExpense Claim #" + claimId + " Totals\n");
+        details.Add("\nTotal travel costs: £" + totalTravelCost);
+        details.Add("Total cost of other expenses: £" + totalExpenses);
+        details.Add("Total cost of expense claim: £" + totalExpenseClaim);
+        details.Add("Total amount that can be reclaimed by company: £" + totalExpenseClaim * 0.2);
+        details.Add("Total non refundable amount: £" + totalNonRefundable);
+        details.Add("Total payment due to employee £" + totalEmployeePayment);
+
+        // details.Add("Non-refundable amount: " + nonRefundable);
+
+
+        // Return the list of receipt details
+        return details;
+    }
+
+}
+
---
+++ expenses-program-v4/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Create a list of the ExpenseClaim objects
+        List<ExpenseClaim> Expenseclaims = new List<ExpenseClaim>();
+
+        // Create a new instance of the ExpenseClaim class
+        ExpenseClaim expenseClaim = new ExpenseClaim();
+
+        // Print Title of program to console
+        
+        Console.Write("\n---------------------NEW EXPENSE CLAIM---------------------\n\n");
+
+
+        // Ask user to set number of journeys in expense claim object
+        expenseClaim.SetNumberOfJourneys();
+
+        // Loop through for the amount of journeys selected by the user for the expense claim instance
+        for (int i = 0; i < expenseClaim.GetNumberOfJourneys(); i++)
+        {
+            // Create a new instance of the Journey class
+            Journey journey = new Journey();
+
+
+            // Get the user to set the claim type of the journey
+            journey.SetClaimType();
+
+            // Get the user to set the travel cost of the current journey
+            journey.SetTravelCost();
+
+            // Add current journey instance expenses to current claim instance running total
+            expenseClaim.AddToTotalTravelCost(journey.GetTravelCost());
+
+            // If claim is for expenses as well as travel then set expense cost 
+
+            if (journey.GetClaimType() == "Travel and Expenses")
+            {
+                // Ask the user to enter the expense cost ,parse to double and save to local variable
+                Console.WriteLine("\nPlease enter the expense cost: ");
+                double expenseCost = double.Parse(Console.ReadLine());
+
+                // Set current journey expense cost from user input
+                journey.SetExpenseCost(expenseCost);
+
+                // Set the non refundable amount in the current journey object
+                journey.SetNonRefundable(expenseCost);
+
+                // Add current journey instance non refundable amount to current claim instance running total
+                expenseClaim.AddToTotalNonRefundable(journey.GetNonRefundable());
+
+                // Add current journey instance expenses to current claim instance running total
+                expenseClaim.AddToTotalExpenses(expenseCost);
+
+                //Add current journey instance expense and travel cost totals to current expense claim instance running total
+                expenseClaim.AddToTotalExpenseClaim();
+
+
+                expenseClaim.SetTotalEmployeePayment();
+
+
+            }
+
+
+
+
+
+            // Add current journey instance to the list of journeys
+            expenseClaim.GetJourneysList().Add(journey);
+
+
+            
+
+        }
+
+        // Print current expense claim receipt details
+
+        Console.Clear();
+        // Print dividing lines x 2
+        for (int j = 0; j < 2; j++)
+        {
+            Console.WriteLine("\n----------------------------------------------------------");
+        }
+
+        Console.WriteLine("\nExpense Claim #" + expenseClaim.GetClaimId() + " Receipt Details");
+
+        Console.WriteLine("\n----------------------------------------------------------");
+
+
+
+        // Print out journey details for each journey in expense claim
+        foreach (var journey in (expenseClaim.GetJourneysList()))
+        {
+            foreach (var detail in (journey.GetReceiptDetails()))
+            {
+                Console.WriteLine(detail);
+                ;
+            }
+            Console.WriteLine("\n---------------------------------------------------------");
+        }
+
+        // Print out expense claim recipt details for whole claim
+
+        Console.WriteLine("\n----------------------------------------------------------");
+
+        foreach (var detail in (expenseClaim.GetReceiptDetails()))
+        {
+            Console.WriteLine(detail);
+        }
+
+        // Print dividing lines x 2
+        for (int j = 0; j < 2; j++)
+        {
+            Console.WriteLine("\n----------------------------------------------------------");
+        }
+
+
+
+
+
+
+    }
+
+}
